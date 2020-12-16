f4e7f30c67652ef57adb6acd10c22927d0ba70d7
Jesús Pallares <jpallares94.jp@gmail.com>
2020-12-16 18:22:17 +0100

    remove unnecessary dist

--- dist/galgus-ng-dcl.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { DclComponent as ɵa } from './src/app/modules/dcl/dcl.component';
--- dist/public_api.d.ts
+++
@@ -1,1 +0,0 @@
-export * from './src/app/modules/dcl/dcl.module';
